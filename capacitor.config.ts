90caa5068a89a65e701e088d8ea7fbf7f799221b
AlesharrV1 <alesharrdev@gmail.com>
2025-07-23 12:12:10 -0400

    Deja de rastrear la carpeta android según .gitignore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'yachariy',
-  webDir: 'www'
+  appId: 'com.yachari.aleshardev',
+  appName: 'yachari',
+  webDir: 'www',
+  plugins: {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": true,
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": true,
+      "splashImmersive": true,
+      "layoutName": "launch_screen",
+      "useDialog": true
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.showSplas();
+  }
+  async showSplas() {
+    await SplashScreen.show({
+      autoHide: true,
+      showDuration: 3000,
+
+    });
+  }
 }
